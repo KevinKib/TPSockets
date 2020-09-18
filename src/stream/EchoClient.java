4601f8e67a010f6cad0228c5e540b66d9c003fb2
Sekanor <sekanor21@gmail.com>
2020-09-18 14:32:28 +0200

    Revert "Revert "Messages entre client et server""
    This reverts commit 3026eda77fb4f566d65310d3077020a5b1f64c04.

--- src/stream/EchoClient.java
+++ src/stream/EchoClient.java
@@ -19,11 +19,6 @@ public class EchoClient {
      **/
     public static void startClient(String[] args) throws IOException {
 
-        Socket echoSocket = null;
-        PrintStream socOut = null;
-        BufferedReader stdIn = null;
-        BufferedReader socIn = null;
-
         if (args.length != 2) {
             System.out.println("Usage: java EchoClient <EchoServer host> <EchoServer port>");
             System.exit(1);
@@ -31,11 +26,53 @@ public class EchoClient {
 
         try {
             // creation socket ==> connexion
-            echoSocket = new Socket(args[0], new Integer(args[1]).intValue());
-            socIn = new BufferedReader(
+            final Socket echoSocket = new Socket(args[0], new Integer(args[1]).intValue());
+            final BufferedReader socIn = new BufferedReader(
                     new InputStreamReader(echoSocket.getInputStream()));
-            socOut = new PrintStream(echoSocket.getOutputStream());
-            stdIn = new BufferedReader(new InputStreamReader(System.in));
+            final PrintStream socOut = new PrintStream(echoSocket.getOutputStream());
+            final BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+
+            Thread read = new Thread(() -> {
+                try {
+                    while (true) {
+                        String line = socIn.readLine();
+                        System.out.println("Server: " + line);
+                    }
+                } catch (Exception e) {
+                    System.err.println("Error in EchoClient:" + e);
+                } finally {
+                    try {
+                        socIn.close();
+                    }
+                    catch (IOException e) {
+                        System.err.println("Failed socket.close()");
+                    }
+                }
+            });
+
+            Thread write = new Thread(() -> {
+                try {
+                    while (true) {
+                        String line = stdIn.readLine();
+                        if (line.equals(".")) break;
+                        socOut.println(line);
+                    }
+                } catch (Exception e) {
+                    System.err.println("Error in EchoClient:" + e);
+                } finally {
+                    try {
+                        stdIn.close();
+                        socOut.close();
+                        echoSocket.close();
+                    }
+                    catch (IOException e) {
+                        System.err.println("Failed socket.close()");
+                    }
+                }
+            });
+
+            read.start();
+            write.start();
         } catch (UnknownHostException e) {
             System.err.println("Don't know about host:" + args[0]);
             System.exit(1);
@@ -44,24 +81,11 @@ public class EchoClient {
                     + "the connection to:" + args[0]);
             System.exit(1);
         }
-
-        String line;
-        while (true) {
-            line = stdIn.readLine();
-            if (line.equals(".")) break;
-            socOut.println(line);
-            System.out.println("echo: " + socIn.readLine());
-        }
-        socOut.close();
-        socIn.close();
-        stdIn.close();
-        echoSocket.close();
     }
 
     /**
      * main method
      *
-     * @param EchoClient port
      **/
     public static void main(String args[]) {
         System.out.println("Client start");
