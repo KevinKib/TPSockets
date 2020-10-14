33d8b8b22cf958a29af485401e7ede81955e87f9
Kévin Kib <sekanor21@gmail.com>
2020-10-14 08:58:23 +0200

    trkl et toi ?

--- HTTP/src/http/server/Handler.java
+++ HTTP/src/http/server/Handler.java
@@ -3,20 +3,25 @@ package http.server;
 import com.google.gson.Gson;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Handler {
 
     private Request request;
     private Response response;
+    private Gson gson;
 
     public Handler() {
         this.request = new Request();
         this.response = new Response();
+        this.gson = new Gson();
     }
 
     protected void readLine(String line) {
@@ -90,23 +95,20 @@ public class Handler {
     }
 
     private void handlePost(OutputStream out, List<Integer> data) {
+        String url = "./src/http/server/users.json";
         StringBuilder userJson = new StringBuilder();
         for (Integer i : data) {
             userJson.append(Character.toChars(i));
         }
 
-        try {
-            File f = new File("./src/http/server/users.json");
-            if (!f.exists()) {
-                f.createNewFile();
-            }
+        if (Files.exists(Paths.get(url))) {
+            ArrayList<User> userList = this.getUsers();
 
             // Add JSON to file
-            Gson g = new Gson();
-            User u = g.fromJson(userJson.toString(), User.class);
+            User u = this.gson.fromJson(userJson.toString(), User.class);
+            userList.add(u);
 
-        } catch(IOException e) {
-
+            this.updateJson(userList);
         }
 
         this.response.setHeader("Content-Type", "");
@@ -123,20 +125,46 @@ public class Handler {
 
     }
 
-    private User[] getUsers() {
-//        User[]
-
+    /**
+     * Retourne une liste d'utilisateurs dans users.json.
+     * @return Liste d'utilisateurs.
+     */
+    private ArrayList<User> getUsers() {
+        String url = this.getFileUrl();
+        String str = "";
+        ArrayList<User> userList = new ArrayList<>();
         try {
-            File f = new File("./src/http/server/users.json");
-            if (!f.exists()) {
-                f.createNewFile();
+            if (Files.exists(Paths.get(url))) {
+                str = new String(Files.readAllBytes(Paths.get(url)));
+                User[] list = this.gson.fromJson(str, User[].class);
+                userList = new ArrayList<>(Arrays.asList(list));
+                System.out.println(userList.get(0).username);
             }
-
         } catch(IOException e) {
-
+            e.printStackTrace();
         }
 
-        return null;
+        return userList;
+    }
+
+    /**
+     * Retourne une liste d'utilisateurs dans users.json.
+     * @return Liste d'utilisateurs.
+     */
+    private void updateJson(ArrayList<User> list) {
+        ArrayList<User> userList = new ArrayList<>();
+        try {
+            FileWriter writer = new FileWriter(this.getFileUrl());
+            String str = this.gson.toJson(list);
+            writer.write(str);
+            writer.close();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String getFileUrl() {
+        return "./src/http/server/users.json";
     }
 
 
